3dfa2512dc00e5c8ca3bd92a5d572e440a95920e
mawusi25 <sirtemple25@gmail.com>
2022-03-04 13:17:36 +0000

    my first react install

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+function Header(){
+  return(
+    <header className="my_header">
+      <nav className="nav">
+        <h1>Ishmael Akaboa</h1>
+        <ul classname="nav-list">
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
